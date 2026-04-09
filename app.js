b55e576806672fdeefbe32466d110424c37ef3cc
Zoibemesa <20241831@itla.edu.do>
2026-04-09 19:54:33 -0400

    fix puerto para render

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const server = http.createServer((req, res) => {
   res.end('Hola Mundo desde Docker 🚀');
 });
 
-server.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
